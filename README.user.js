a8e26cd46818976593d0b5dd0aa3147f55d8e862
Samintha Kaveesh <skaveesh@gmail.com>
2017-11-20 22:48:03 +0530

    Update README.user.js

--- README.user.js
+++ README.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Elakiri Quick Reply Advanced
+// @name         EKQR Advanced
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
